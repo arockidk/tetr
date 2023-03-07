c3ef2c5a39c1054615573e71cfdd0fbba89cfcd6
arockidk <joeykicker2018@gmail.com>
2023-03-07 21:27:14 +1100

    Rotation, hard dropping and 7Bag
    Implemented basic SRS rotations, with no kicks yet. Implemented placing blocks and hard dropping. Implemented the "7Bag" rng system.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var tile_color_array = ["z", "s", "l", "j", "i", "t", "o", "b", "g"];
 var tiles = [];
+var colors = ["t", "o", "z", "s", "i", "j", "l"];
 var block_masks = {
     "t": [
         [[0, 1, 0], [1, 1, 1], [0, 0, 0]],
@@ -9,25 +19,56 @@ var block_masks = {
     ],
     "z": [
         [[1, 1, 0], [0, 1, 1], [0, 0, 0]],
+        [[0, 0, 1], [0, 1, 1], [0, 1, 0]],
+        [[0, 0, 0], [1, 1, 0], [0, 1, 1]],
+        [[0, 1, 0], [1, 1, 0], [1, 0, 0]],
     ],
     "l": [
         [[0, 0, 1], [1, 1, 1], [0, 0, 0]],
+        [[0, 1, 0], [0, 1, 0], [0, 1, 1]],
+        [[0, 0, 0], [1, 1, 1], [1, 0, 0]],
+        [[1, 1, 0], [0, 1, 0], [0, 1, 0]],
     ],
     "s": [
         [[0, 1, 1], [1, 1, 0], [0, 0, 0]],
+        [[1, 0, 0], [1, 1, 0], [0, 1, 0]],
+        [[0, 0, 0], [0, 1, 1], [1, 1, 0]],
+        [[0, 1, 0], [0, 1, 1], [0, 0, 1]],
     ],
     "j": [
         [[1, 0, 0], [1, 1, 1], [0, 0, 0]],
+        [[0, 1, 1], [0, 1, 0], [0, 1, 0]],
+        [[0, 0, 0], [1, 1, 1], [0, 0, 1]],
+        [[0, 1, 0], [0, 1, 0], [1, 1, 0]],
     ],
     "o": [
-        [[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]
+        [[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+        [[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+        [[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+        [[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
     ],
     "i": [
-        [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]]
+        [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+        [[0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0]],
+        [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]],
+        [[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]],
     ]
 };
 var ROWS = 20;
 var COLUMNS = 10;
+var random = function (min, max) { return Math.random() * (max - min) + min; };
+function shuffle(array) {
+    var _a;
+    var currentIndex = array.length, randomIndex;
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        _a = [
+            array[randomIndex], array[currentIndex]
+        ], array[currentIndex] = _a[0], array[randomIndex] = _a[1];
+    }
+    return array;
+}
 function setTileColor(tile, color) {
     tile.color = color;
     for (var _i = 0, tile_color_array_1 = tile_color_array; _i < tile_color_array_1.length; _i++) {
@@ -58,6 +99,10 @@ function getSolidsFromRectangle(topLeft, size) {
     for (var row = 0; row < size.y; row++) {
         solids[row] = [];
         for (var col = 0; col < size.x; col++) {
+            if (topLeft.x + col > COLUMNS - 1 || topLeft.x + col < 0) {
+                solids[row][col] = 1;
+                continue;
+            }
             var itile = getITileAt(topLeft.x + col, topLeft.y + row);
             try {
                 solids[row][col] = itile.color === "b" ? 0 : 1;
@@ -185,11 +230,30 @@ function deleteBlock(block_color, topLeft, rot_state) {
         r += 1;
     }
 }
-function moveBlock(block_color, topLeft, rot_state, vector) {
-    var new_pos = { x: topLeft.x + vector.x, y: topLeft.y + vector.y };
-    if (new_pos.x < 0 || new_pos.x > COLUMNS - 3) {
+function rotateBlock(block_color, topLeft, old, New) {
+    deleteBlock(block_color, topLeft, old);
+    var mask = block_masks[block_color][New];
+    // let solids = getSolidsFromRectangle(topLeft, {x: mask[0].length, y: mask.length});
+    // console.table(solids);
+    var success = !checkColision(block_color, New, topLeft);
+    // console.log(success, "EE");
+    if (success) {
+        // try {
+        //     createBlock(block_color, new_pos, rot_state);
+        // } catch (error) {
+        // }
+        createBlock(block_color, topLeft, New);
+        // console.log("success");
+        return true;
+    }
+    else {
+        createBlock(block_color, topLeft, old);
+        // deleteBlock(block_color, new_pos, rot_state);
         return false;
     }
+}
+function moveBlock(block_color, topLeft, rot_state, vector) {
+    var new_pos = { x: topLeft.x + vector.x, y: topLeft.y + vector.y };
     deleteBlock(block_color, topLeft, rot_state);
     var mask = block_masks[block_color][rot_state];
     // let solids = getSolidsFromRectangle(topLeft, {x: mask[0].length, y: mask.length});
@@ -211,38 +275,74 @@ function moveBlock(block_color, topLeft, rot_state, vector) {
         return false;
     }
 }
+function rotateCW(old) {
+    return (old >= 3 ? 0 : old + 1);
+}
+function rotateCCW(old) {
+    return (old <= 0 ? 3 : old - 1);
+}
 function createTiles() {
     var grid = document.getElementsByClassName("grid")[0];
     for (var i = 0; i < ROWS * COLUMNS; i++) {
         grid.appendChild(createTile("b").element);
     }
 }
+function generateRandomColor() {
+    var ra = Math.floor(random(0, 7));
+    var color = colors[ra];
+    console.log(color);
+    return color;
+}
+var generate7Bag = function () { return shuffle(colors.map(function (v) { return v; })); };
 var rot_state = 0;
 var piece_spawned = false;
 var cur_top_left = { x: 1, y: 0 };
 var keys = {
     "left": false,
     "right": false,
-    "up": false,
-    "down": false
+    "cw": false,
+    "ccw": false,
+    "sd": false,
+    "hd": false
 };
 var SPAWN_POS = { x: 4, y: 0 };
 var STANDARD_GRAVITY = 1 / 64;
-var SOFT_DROP_FACTOR = 62220;
+var DAS = 10;
+var SOFT_DROP_FACTOR = 20;
+var GROUNDED_TIME = 30;
 var GRAVITY = STANDARD_GRAVITY;
+var placed = 0;
 var gravity_complete = 0;
+var grounded = 0;
+var das_timer = 0;
+var debounce = {
+    "CW": false,
+    "CCW": false,
+    "HD": false
+};
+var cur_color;
+var next = __spreadArray(__spreadArray([], generate7Bag(), true), generate7Bag(), true);
 window.onload = function () {
     createTiles();
     var board_state;
-    createBlock("o", { x: 1, y: 16 }, 0);
+    // createBlock("o", {x:1, y:16}, 0)
     setInterval(function () {
         if (!piece_spawned) {
-            createBlock("t", SPAWN_POS, 0);
-            cur_top_left = SPAWN_POS;
+            rot_state = 0;
+            // console.log("Spawning")
+            cur_color = next.shift();
+            createBlock(cur_color, { x: 4, y: 0 }, rot_state);
+            cur_top_left = { x: 4, y: 0 };
+            // console.log(cur_top_left,SPAWN_POS);
             piece_spawned = true;
+            placed++;
+            if (placed % 7 === 0) {
+                next.push.apply(next, generate7Bag());
+            }
+            console.log(next);
         }
         try {
-            if (keys.down) {
+            if (keys.sd) {
                 GRAVITY = STANDARD_GRAVITY * SOFT_DROP_FACTOR;
             }
             else {
@@ -251,29 +351,71 @@ window.onload = function () {
             if (gravity_complete >= 1) {
                 var move = Math.floor(gravity_complete);
                 gravity_complete -= Math.floor(gravity_complete);
-                if (moveBlock("t", cur_top_left, 0, { x: 0, y: move })) {
-                    cur_top_left.y += move;
-                    console.log(cur_top_left);
+                for (var i = 0; i < move; i++) {
+                    if (moveBlock(cur_color, cur_top_left, rot_state, { x: 0, y: 1 })) {
+                        cur_top_left.y += 1;
+                        console.log(cur_top_left);
+                    }
                 }
             }
             else {
                 gravity_complete += GRAVITY;
             }
             if (keys.right) {
-                if (moveBlock("t", cur_top_left, 0, { x: 1, y: 0 })) {
-                    cur_top_left.x += 1;
+                if (das_timer === 0 || das_timer > DAS) {
+                    if (moveBlock(cur_color, cur_top_left, rot_state, { x: 1, y: 0 })) {
+                        cur_top_left.x += 1;
+                    }
                 }
+                das_timer += 1;
             }
             else if (keys.left) {
-                if (moveBlock("t", cur_top_left, 0, { x: -1, y: 0 })) {
-                    cur_top_left.x -= 1;
+                if (das_timer === 0 || das_timer > 9) {
+                    if (moveBlock(cur_color, cur_top_left, rot_state, { x: -1, y: 0 })) {
+                        cur_top_left.x -= 1;
+                    }
                 }
+                das_timer += 1;
+            }
+            else if (keys.cw && !debounce.CW) {
+                if (rotateBlock(cur_color, cur_top_left, rot_state, rotateCW(rot_state))) {
+                    rot_state = rotateCW(rot_state);
+                    debounce.CW = true;
+                }
+            }
+            else if (keys.ccw && !debounce.CCW) {
+                if (rotateBlock(cur_color, cur_top_left, rot_state, rotateCCW(rot_state))) {
+                    rot_state = rotateCCW(rot_state);
+                    debounce.CCW = true;
+                }
+            }
+            if (!(keys.left || keys.right)) {
+                das_timer = 0;
+            }
+            if (keys.hd && !debounce.HD) {
+                while (moveBlock(cur_color, cur_top_left, rot_state, { x: 0, y: 1 })) {
+                    cur_top_left.y += 1;
+                }
+                grounded = Infinity;
+                debounce.HD = true;
             }
         }
         catch (error) {
         }
         // console.log("y =", cur_top_left.y);
-        // console.log(cur_top_left);
+        deleteBlock(cur_color, cur_top_left, rot_state);
+        if (checkColision(cur_color, rot_state, { x: cur_top_left.x, y: cur_top_left.y + 1 })) {
+            grounded += 1;
+        }
+        else {
+            grounded = 0;
+        }
+        createBlock(cur_color, cur_top_left, rot_state);
+        if (grounded >= GROUNDED_TIME) {
+            // console.log("EE")
+            piece_spawned = false;
+            grounded = 0;
+        }
     }, 1000 / 60);
 };
 window.onkeydown = function (e) {
@@ -285,7 +427,16 @@ window.onkeydown = function (e) {
         keys.left = true;
     }
     else if (e.keyCode == 40) {
-        keys.down = true;
+        keys.sd = true;
+    }
+    else if (e.keyCode == 38) {
+        keys.cw = true;
+    }
+    else if (e.key == "a") {
+        keys.ccw = true;
+    }
+    else if (e.key == " ") {
+        keys.hd = true;
     }
 };
 window.onkeyup = function (e) {
@@ -297,6 +448,18 @@ window.onkeyup = function (e) {
         keys.left = false;
     }
     else if (e.keyCode == 40) {
-        keys.down = false;
+        keys.sd = false;
+    }
+    else if (e.keyCode == 38) {
+        keys.cw = false;
+        debounce.CW = false;
+    }
+    else if (e.key == "a") {
+        keys.ccw = false;
+        debounce.CCW = false;
+    }
+    else if (e.key == " ") {
+        keys.hd = false;
+        debounce.HD = false;
     }
 };
--- script.ts
+++ script.ts
@@ -10,6 +10,7 @@ interface ITile {
 } 
 
 let tiles: ITile[] = [];
+let colors = ["t", "o", "z", "s", "i", "j", "l"]
 const block_masks: {[K in block_color]: int[][][]} = {
     "t": [
         [[0,1,0],[1,1,1],[0, 0, 0]],
@@ -20,34 +21,61 @@ const block_masks: {[K in block_color]: int[][][]} = {
     ],
     "z": [
         [[1,1,0],[0,1,1],[0, 0, 0]],
-
-
+        [[0,0,1],[0,1,1],[0, 1, 0]],
+        [[0, 0, 0],[1,1,0],[0,1,1]],
+        [[0,1,0],[1,1,0],[1, 0, 0]],
+        
     ],
     "l": [
         [[0,0,1],[1,1,1],[0, 0, 0]],
-
-
+        [[0,1,0],[0,1,0],[0, 1, 1]],
+        [[0,0,0],[1,1,1],[1, 0, 0]],
+        [[1,1,0],[0,1,0],[0, 1, 0]],
     ],
     "s": [
         [[0,1,1],[1,1,0],[0, 0, 0]],
-
-
+        [[1,0,0],[1,1,0],[0, 1, 0]],
+        [[0, 0, 0],[0,1,1],[1,1,0]],
+        [[0,1,0],[0,1,1],[0, 0, 1]],
     ],
     "j": [
         [[1,0,0],[1,1,1],[0, 0, 0]],
-
-
+        [[0,1,1],[0,1,0],[0, 1, 0]],
+        [[0,0,0],[1,1,1],[0, 0, 1]],
+        [[0,1,0],[0,1,0],[1, 1, 0]],
     ],
     "o": [
-        [[0,1,1,0],[0,1,1,0],[0,0,0,0]]
+        [[0,1,1,0],[0,1,1,0],[0,0,0,0]],
+        [[0,1,1,0],[0,1,1,0],[0,0,0,0]],
+        [[0,1,1,0],[0,1,1,0],[0,0,0,0]],
+        [[0,1,1,0],[0,1,1,0],[0,0,0,0]],
     ],
     "i": [
-        [[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]]
+        [[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]],
+        [[0,0,1,0],[0,0,1,0],[0,0,1,0],[0,0,1,0]],
+        [[0,0,0,0],[0,0,0,0],[1,1,1,1],[0,0,0,0]],
+        [[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]],
     ]
     
 }
 const ROWS = 20;
 const COLUMNS = 10;
+const random = (min: int, max: int):int => Math.random() * (max - min) + min;
+function shuffle<T>(array: T[]): T[] {
+    let currentIndex = array.length,  randomIndex: int;
+  
+    while (currentIndex != 0) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 function setTileColor(tile: ITile, color: tile_color): ITile {
     tile.color = color;
     for (let color of tile_color_array) {
@@ -82,6 +110,10 @@ function getSolidsFromRectangle(topLeft: {x: number, y: number}, size: {x: numbe
     for (let row = 0; row < size.y; row++) {
         solids[row] = [];
         for (let col = 0; col < size.x; col++) {
+            if (topLeft.x + col > COLUMNS - 1 || topLeft.x + col < 0) {
+                solids[row][col] = 1;
+                continue;
+            }
             let itile: ITile = getITileAt(topLeft.x + col, topLeft.y + row);
             try {
                 solids[row][col] = itile.color === "b" ? 0 : 1;
@@ -160,7 +192,7 @@ function formattedArray(array: any[]): string {
 function checkColision(block_color: block_color, block_rotation: rotation_state,tl: {x: number, y: number}):boolean {
     let mask = block_masks[block_color][block_rotation];
     let solids = getSolidsFromRectangle(tl, {x: mask[0].length, y: mask.length});
-
+    
     let result = bitwiseMatrixComparison(mask,solids,"and") 
     let cont = matrixContains<int>(result, 1);
     
@@ -221,12 +253,38 @@ function deleteBlock(block_color: block_color, topLeft: {x: number,y:number}, ro
     }
 
 }
+function rotateBlock(block_color: block_color, topLeft: {x: number,y: number}, old: rotation_state, New: rotation_state):boolean  {
+
+    deleteBlock(block_color, topLeft, old);
+    let mask = block_masks[block_color][New];
+    // let solids = getSolidsFromRectangle(topLeft, {x: mask[0].length, y: mask.length});
+    // console.table(solids);
+    let success: boolean = !checkColision(block_color, New, topLeft);
+    // console.log(success, "EE");
+
+    if (success) {
+        // try {
+        //     createBlock(block_color, new_pos, rot_state);
+        // } catch (error) {
+            
+        // }
+        createBlock(block_color, topLeft, New);
+        // console.log("success");
+        return true;
+        
+        
+    } else {
+        createBlock(block_color, topLeft, old);
+        // deleteBlock(block_color, new_pos, rot_state);
+        return false;
+
+    }
+
+
+}
 function moveBlock(block_color: block_color, topLeft: {x: number,y: number}, rot_state: rotation_state, vector: {x: number, y: number}):boolean  {
     let new_pos = {x: topLeft.x + vector.x, y: topLeft.y + vector.y};
     
-    if (new_pos.x < 0 || new_pos.x > COLUMNS - 3 ) {
-        return false;
-    }
     deleteBlock(block_color, topLeft, rot_state);
     let mask = block_masks[block_color][rot_state];
     // let solids = getSolidsFromRectangle(topLeft, {x: mask[0].length, y: mask.length});
@@ -254,7 +312,12 @@ function moveBlock(block_color: block_color, topLeft: {x: number,y: number}, rot
 
 
 }
-
+function rotateCW(old: rotation_state): rotation_state {
+    return (old >= 3 ? 0 : old + 1) as rotation_state;
+}
+function rotateCCW(old: rotation_state): rotation_state {
+    return (old <= 0 ? 3 : old - 1) as rotation_state;
+}
 function createTiles() {
     let grid = document.getElementsByClassName("grid")[0];
     
@@ -262,35 +325,65 @@ function createTiles() {
         grid.appendChild(createTile("b").element);
     }
 }
+function generateRandomColor(): block_color {
+    let ra = Math.floor(random(0,7));
+    let color = colors[ra] as block_color;
+    console.log(color);
+    return color;
+} 
+const generate7Bag = (): block_color[] => shuffle(colors.map((v)=>v)) as block_color[];
 let rot_state: rotation_state = 0;
 let piece_spawned: boolean = false;
 let cur_top_left: {x: number, y: number} = {x: 1, y: 0}
 let keys = {
     "left": false,
     "right": false,
-    "up": false,
-    "down": false
+    "cw": false,
+    "ccw": false,
+    "sd": false,
+    "hd": false,
 }
 const SPAWN_POS = {x: 4, y: 0};
 const STANDARD_GRAVITY = 1/64;
-let SOFT_DROP_FACTOR = 62220;
-
+let DAS = 10;
+let SOFT_DROP_FACTOR = 20;
+let GROUNDED_TIME = 30;
 let GRAVITY = STANDARD_GRAVITY;
-let gravity_complete = 0
+let placed = 0;
+let gravity_complete = 0;
+let grounded = 0;
+let das_timer = 0;
+let debounce = {
+    "CW": false,
+    "CCW": false,
+    "HD": false
+};
+let cur_color: block_color;
+let next = [...generate7Bag(),...generate7Bag()];
 window.onload = function() {
         createTiles();
         let board_state;
-        createBlock("o", {x:1, y:16}, 0)
+        // createBlock("o", {x:1, y:16}, 0)
         setInterval(function() {
             if (!piece_spawned) {
-                
-                createBlock("t",SPAWN_POS,0); 
-                cur_top_left = SPAWN_POS;
+                rot_state = 0;
+                // console.log("Spawning")
+                cur_color = next.shift();
+                createBlock(cur_color,{x: 4, y: 0}, rot_state); 
+                cur_top_left = {x: 4, y: 0};
+                // console.log(cur_top_left,SPAWN_POS);
                 piece_spawned = true;
+                placed++;
+
+                if (placed % 7 === 0) {
+                    next.push(...generate7Bag());
+                }
+                console.log(next)
             }
 
             try {
-                if (keys.down) {
+                
+                if (keys.sd) {
                     GRAVITY = STANDARD_GRAVITY * SOFT_DROP_FACTOR;
                 } else {
                     GRAVITY = STANDARD_GRAVITY;
@@ -298,23 +391,55 @@ window.onload = function() {
                 if (gravity_complete >= 1) {
                     let move = Math.floor(gravity_complete);
                     gravity_complete -= Math.floor(gravity_complete);
-                    if (moveBlock("t", cur_top_left, 0, {x:0,y:move})) {
-                        cur_top_left.y += move;
-                        console.log(cur_top_left)
-                    }
+                    for (let i = 0; i < move; i++) {
+                        if (moveBlock(cur_color, cur_top_left, rot_state, {x:0,y:1})) {
+                            cur_top_left.y += 1;
+                            console.log(cur_top_left)
+                        }
+                    }   
                 } else {
                     gravity_complete += GRAVITY;
                 }
+                
 
                 if (keys.right) {
-                    if (moveBlock("t", cur_top_left, 0, {x:1,y:0})) {
-                        cur_top_left.x += 1;
-                    }
+                    if (das_timer === 0 || das_timer > DAS) {
+                        if (moveBlock(cur_color, cur_top_left, rot_state, {x:1,y:0})) {
+                            cur_top_left.x += 1;
+                        }
+                        
+                    } 
+                    das_timer += 1;
                 } else if (keys.left) {
-                    if (moveBlock("t", cur_top_left, 0, {x:-1,y:0})) {
-                        cur_top_left.x -= 1;
+                    if (das_timer === 0 || das_timer > 9) {
+                        if (moveBlock(cur_color, cur_top_left, rot_state, {x:-1,y:0})) {
+                            cur_top_left.x -= 1;
+                        }
+                        
+                    } 
+                    das_timer += 1;
+                } else if (keys.cw && !debounce.CW) {
+                    if (rotateBlock(cur_color, cur_top_left, rot_state, rotateCW(rot_state))) {
+                        rot_state = rotateCW(rot_state);
+                        debounce.CW = true;
                     }
                 } 
+                else if (keys.ccw && !debounce.CCW) {
+                    if (rotateBlock(cur_color, cur_top_left, rot_state, rotateCCW(rot_state))) {
+                        rot_state = rotateCCW(rot_state);
+                        debounce.CCW = true;
+                    }
+                } 
+                if (!(keys.left || keys.right)) {
+                    das_timer = 0;
+                }
+                if (keys.hd && !debounce.HD) {
+                    while (moveBlock(cur_color, cur_top_left, rot_state, {x:0,y:1})) {
+                        cur_top_left.y += 1;
+                    }
+                    grounded = Infinity;
+                    debounce.HD = true;
+                }
             } catch (error) {
                 
             }
@@ -322,7 +447,20 @@ window.onload = function() {
 
 
             // console.log("y =", cur_top_left.y);
-            // console.log(cur_top_left);
+            deleteBlock(cur_color,cur_top_left,rot_state);
+            if (checkColision(cur_color,rot_state,{x: cur_top_left.x, y: cur_top_left.y + 1})) {
+                grounded += 1;
+            } else {
+                grounded = 0;
+            }
+            createBlock(cur_color,cur_top_left,rot_state);
+            if (grounded >= GROUNDED_TIME) {
+                // console.log("EE")
+                piece_spawned = false;
+                
+                grounded = 0;
+            } 
+
         },1000/60)
         
 
@@ -335,7 +473,16 @@ window.onkeydown = function(e: KeyboardEvent) {
         keys.left = true;
     }
     else if (e.keyCode == 40) {
-        keys.down = true;
+        keys.sd = true;
+    }
+    else if (e.keyCode == 38) {
+        keys.cw = true;
+    }
+    else if (e.key == "a") {
+        keys.ccw = true;
+    }
+    else if (e.key == " ") {
+        keys.hd = true;
     }
 }
 window.onkeyup = function(e: KeyboardEvent) {
@@ -345,6 +492,18 @@ window.onkeyup = function(e: KeyboardEvent) {
     } else if (e.keyCode == 37) {
         keys.left = false;
     } else if (e.keyCode == 40) {
-        keys.down = false;
+        keys.sd = false;
+    }
+    else if (e.keyCode == 38) {
+        keys.cw = false;
+        debounce.CW = false;
+    }
+    else if (e.key == "a") {
+        keys.ccw = false;
+        debounce.CCW = false;
+    }
+    else if (e.key == " ") {
+        keys.hd = false;
+        debounce.HD = false;
     }
 }
